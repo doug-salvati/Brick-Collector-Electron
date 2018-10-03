059c7db03008a27e92f61520fa7e88f716ae5239
doug-salvati <dougiesal@gmail.com>
2018-10-03 18:06:25 -0400

    :bento: use png for assets

--- src/data/themeimages.js
+++ src/data/themeimages.js
@@ -1,9 +1,10 @@
 const ThemeImages = {
-    "Minecraft": "set_images/themes/minecraft.jpg",
-    "Star Wars": "set_images/themes/starwars.jpg",
-    "Star Wars Episode 4/5/6": "set_images/themes/starwars.jpg",
-    "Ultimate Collector Series": "set_images/themes/starwars.jpg",
-    "Jungle": "set_images/themes/cityjungle.jpg"
+    "Minecraft": "set_images/themes/minecraft.png",
+    "Star Wars": "set_images/themes/starwars.png",
+    "Star Wars Episode 4/5/6": "set_images/themes/starwars.png",
+    "Ultimate Collector Series": "set_images/themes/starwars.png",
+    "Jungle": "set_images/themes/cityjungle.png",
+    "Police": "set_images/themes/citypolice.png"
 };
 
 export default ThemeImages;

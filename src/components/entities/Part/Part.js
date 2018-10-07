75fec6718ee5231907cb327f63ab725be41a0510
doug-salvati <dougiesal@gmail.com>
2018-10-07 15:19:19 -0400

    :bug: restore missing item quantities

--- src/components/entities/Part/Part.js
+++ src/components/entities/Part/Part.js
@@ -3,11 +3,11 @@ import ColorHex from '../../../data/colors.js';
 import Entity from '../Entity/Entity';
 
 const Part = (props) => {
-    const {qty, classification, ...rest} = props;
+    const {qty, classification, number, ...rest} = props;
     return (
         <Entity
             imagepath="assets/part_images/elements"
-            number={qty ? `${qty}x ` : undefined}
+            number={number || (qty ? `${qty}x ` : undefined)}
             icon={ColorHex[classification]}
             iconTooltip={classification}
             {...rest}
